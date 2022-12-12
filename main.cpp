44ea56f5746ab9a08491c0d5810cbc73c51332fe
andreabeverini <117717829+andreabeverini@users.noreply.github.com>
2022-12-12 15:15:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+int n;
+cin>>n;
+float a[n];
+float x;
+for(int i=0; i<n; i++){
+    cin>>x;
+    a[i]=x;
+}
+cout<<max(a,n);
+    return 0;
 }
